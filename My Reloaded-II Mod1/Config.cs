11ba9a2dc9024a12866f89c26789797c4b58c738
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2023-08-01 13:05:00 -0400

    Add project files.

---
+++ My Reloaded-II Mod1/Config.cs
@@ -0,0 +1,109 @@
+﻿using My_Reloaded_II_Mod1.Template.Configuration;
+using Reloaded.Mod.Interfaces.Structs;
+using System.ComponentModel;
+
+namespace My_Reloaded_II_Mod1.Configuration
+{
+	public class Config : Configurable<Config>
+	{
+		/*
+			User Properties:
+				- Please put all of your configurable properties here.
+
+			By default, configuration saves as "Config.json" in mod user config folder.    
+			Need more config files/classes? See Configuration.cs
+
+			Available Attributes:
+			- Category
+			- DisplayName
+			- Description
+			- DefaultValue
+
+			// Technically Supported but not Useful
+			- Browsable
+			- Localizable
+
+			The `DefaultValue` attribute is used as part of the `Reset` button in Reloaded-Launcher.
+		*/
+
+		[DisplayName("Next Chance to Move On")]
+		[Description("Enable to play Peak.")]
+		[DefaultValue(false)]
+		public bool NextChanceToMoveOn { get; set; } = false;
+
+		[DisplayName("Shadow World")]
+		[Description("Enable to play Shadow World from Persona 4 Golden Animation.")]
+		[DefaultValue(false)]
+		public bool ShadowWorld { get; set; } = false;
+
+		[DisplayName("Skys The Limit")]
+		[Description("Enable to play Peak 2.")]
+		[DefaultValue(false)]
+		public bool SkysTheLimit { get; set; } = false;
+
+		[DisplayName("Key Plus Words")]
+		[Description("Enable to play Peak Key Plus Words.")]
+		[DefaultValue(false)]
+		public bool KeyPlusWords { get; set; } = false;
+
+		[DisplayName("Key Plus Words Alt Subs")]
+		[Description("Enable to play Key Plus Words with Alt Subs.")]
+		[DefaultValue(false)]
+		public bool KeyPlusWordsAltSubtitles { get; set; } = false;
+
+		[DisplayName("Vanilla Shadow World")]
+		[Description("Enable to play Good Song :).")]
+		[DefaultValue(false)]
+		public bool ShadowWorldVanilla { get; set; } = false;
+
+		[DisplayName("Pursuing My True Self")]
+		[Description("Enable to play Pursuing My True Self.")]
+		[DefaultValue(false)]
+		public bool P4Intro { get; set; } = false;
+
+		[DisplayName("Dance!")]
+		[Description("Enable to play P4D Intro, Dance!")]
+		[DefaultValue(false)]
+		public bool Dance { get; set; } = false;
+
+		[DisplayName("P4AU")]
+		[Description("Enable to play P4AU Intro.")]
+		[DefaultValue(false)]
+		public bool P4AU { get; set; } = false;
+
+		[DisplayName("P4A")]
+		[Description("Enable to play P4A Intro.")]
+		[DefaultValue(false)]
+		public bool P4A { get; set; } = false;
+
+		[DisplayName("PQ")]
+		[Description("Enable to play PQ Intro.")]
+		[DefaultValue(false)]
+		public bool PQ { get; set; } = false;
+
+		[DisplayName("PQ2")]
+		[Description("Enable to play PQ2 Intro.")]
+		[DefaultValue(false)]
+		public bool PQ2 { get; set; } = false;
+
+		[DisplayName("Enum")]
+		[Description("This is an enumerable.")]
+		[DefaultValue(SampleEnum.Normal)]
+		public SampleEnum Reloaded { get; set; } = SampleEnum.Normal;
+
+		public enum SampleEnum
+		{
+			Normal,
+			Random,
+		}
+	}
+
+	/// <summary>
+	/// Allows you to override certain aspects of the configuration creation process (e.g. create multiple configurations).
+	/// Override elements in <see cref="ConfiguratorMixinBase"/> for finer control.
+	/// </summary>
+	public class ConfiguratorMixin : ConfiguratorMixinBase
+	{
+		// 
+	}
+}
---
+++ My Reloaded-II Mod1/Mod.cs
@@ -0,0 +1,127 @@
+﻿using CriFs.V2.Hook;
+using CriFs.V2.Hook.Interfaces;
+using My_Reloaded_II_Mod1.Configuration;
+using My_Reloaded_II_Mod1.Template;
+using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Mod.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace My_Reloaded_II_Mod1
+{
+	public class Mod : ModBase
+	{
+		private readonly IModLoader _modLoader;
+		private readonly IReloadedHooks? _hooks;
+		private readonly ILogger _logger;
+		private readonly IMod _owner;
+		private Config _configuration;
+		private readonly IModConfig _modConfig;
+
+		// Track the last played movie
+		private static string _lastPlayedMovie;
+
+		public Mod(ModContext context)
+		{
+			_modLoader = context.ModLoader;
+			_hooks = context.Hooks;
+			_logger = context.Logger;
+			_owner = context.Owner;
+			_configuration = context.Configuration;
+			_modConfig = context.ModConfig;
+
+			var criFsController = _modLoader.GetController<ICriFsRedirectorApi>();
+			if (criFsController == null || !criFsController.TryGetTarget(out var criFsApi))
+			{
+				_logger.WriteLine($"Unable to get controller for CriFs Lib, things will not work :(", System.Drawing.Color.Red);
+				return;
+			}
+
+			// Create a list to hold the enabled movie options
+			List<string> enabledMovies = new List<string>();
+
+			// Check each bool and add the corresponding movie option to the list if enabled
+			if (_configuration.NextChanceToMoveOn && _lastPlayedMovie != "NextChanceToMoveOn")
+			{
+				enabledMovies.Add("NextChanceToMoveOn");
+			}
+
+			if (_configuration.ShadowWorld && _lastPlayedMovie != "ShadowWorld")
+			{
+				enabledMovies.Add("ShadowWorld");
+			}
+
+			if (_configuration.SkysTheLimit && _lastPlayedMovie != "SkysTheLimit")
+			{
+				enabledMovies.Add("SkysTheLimit");
+			}
+
+			if (_configuration.KeyPlusWords && _lastPlayedMovie != "KeyPlusWords")
+			{
+				enabledMovies.Add("KeyPlusWords");
+			}
+
+			if (_configuration.KeyPlusWordsAltSubtitles && _lastPlayedMovie != "KeyPlusWordsAltSubtitles")
+			{
+				enabledMovies.Add("KeyPlusWordsAltSubtitles");
+			}
+
+			if (_configuration.ShadowWorldVanilla && _lastPlayedMovie != "ShadowWorldVanilla")
+			{
+				enabledMovies.Add("ShadowWorldVanilla");
+			}
+
+			if (_configuration.P4Intro && _lastPlayedMovie != "P4Intro")
+			{
+				enabledMovies.Add("P4Intro");
+			}
+
+			if (_configuration.Dance && _lastPlayedMovie != "Dance")
+			{
+				enabledMovies.Add("Dance");
+			}
+
+			if (_configuration.P4AU && _lastPlayedMovie != "P4AU")
+			{
+				enabledMovies.Add("P4AU");
+			}
+
+			if (_configuration.P4A && _lastPlayedMovie != "P4A")
+			{
+				enabledMovies.Add("P4A");
+			}
+
+			if (_configuration.PQ && _lastPlayedMovie != "PQ")
+			{
+				enabledMovies.Add("PQ");
+			}
+
+			if (_configuration.PQ2 && _lastPlayedMovie != "PQ2")
+			{
+				enabledMovies.Add("PQ2");
+			}
+
+			// Randomly select a movie from the enabled options
+			if (enabledMovies.Count > 0)
+			{
+				Random random = new Random();
+				string selectedMovie = enabledMovies[random.Next(0, enabledMovies.Count)];
+				_lastPlayedMovie = selectedMovie; // Update the last played movie
+				criFsApi.AddProbingPath(selectedMovie);
+			}
+		}
+
+		#region Standard Overrides
+		public override void ConfigurationUpdated(Config configuration)
+		{
+			_configuration = configuration;
+			_logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
+		}
+		#endregion
+
+		#region For Exports, Serialization etc.
+		public Mod() { }
+		#endregion
+	}
+}
